dd93413e676c040350090d39f03b2dce28e6e4f2
Manuel Eiweck <manuel.eiweck@bkf.at>
2020-12-06 22:45:28 +0100

    add basic HTML/CSS Layout
    using “Holy Grail Layout” Flexboxlayout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Vtuber Dashboard</h1>
       </header>
+      <main>
+        <nav>
+          <ul>
+            <li>Menu1</li>
+            <li>Menu2</li>
+            <li>Menu3</li>
+          </ul>
+        </nav>
+        <article>
+          <h3>List of live VTuber</h3>
+          <ul>
+            <li>VTuber1</li>
+            <li>VTuber2</li>
+            <li>VTuber3</li>
+          </ul>
+        </article>
+        <aside>
+          <ul>
+            <li>Side1</li>
+            <li>Side2</li>
+            <li>Side3</li>
+          </ul>
+        </aside>
+      </main>
+      <footer>
+        <h4>Footer text</h4>
+      </footer>
     </div>
   );
 }
